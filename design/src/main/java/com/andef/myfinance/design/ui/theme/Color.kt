298bc76a129f09b342c080fc56ba37f175365852
dsemkin <semkin_dmitriy10@vk.com>
2025-06-15 17:51:17 +0400

    DI start

--- design/src/main/java/com/andef/myfinance/design/ui/theme/Color.kt
+++ design/src/main/java/com/andef/myfinance/design/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.andef.myfinance.design.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF0A0A0A)
+val LightBlack = Color(0xFF111111)
+val DarkGray = Color(0xFF444444)
+val Gray = Color(0xFF888888)
+val Blue = Color(0xFF1D62CC)
+val Red = Color(0xFFC91111)
--- design/src/main/java/com/andef/myfinance/design/ui/theme/Theme.kt
+++ design/src/main/java/com/andef/myfinance/design/ui/theme/Theme.kt
@@ -11,25 +11,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LightBlack,
+    secondary = LightBlack,
+    tertiary = LightBlack,
+    background = LightBlack,
+    surface = LightBlack,
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = LightBlack,
+    secondary = LightBlack,
+    tertiary = LightBlack,
+    background = LightBlack,
+    surface = LightBlack,
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onBackground = White,
+    onSurface = White
 )
 
 @Composable
